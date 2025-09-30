64f08018fef8e7c3836a1a5654ccbeba39d6a98b
mstfkaradag <mustafakaradag20200@gmail.com>
2025-10-01 00:25:54 +0300

    Coded rotations for encryption and decryption

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template, jsonify
+from encryption.caesar import caesar_encrypt, caesar_decrypt
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/encrypt', methods = ['POST'])
+def encrypt():
+    data = request.json
+    message = data['message']
+    key = int(data['key'])
+    method = data['method']
+
+    if method == 'caesar':
+        result = caesar_encrypt(message, key)
+
+    return jsonify({'encrypted_message': result})
+
+@app.route('/decrypt', method = ['POST'])
+def decrypt():
+    data = request.json
+    message = data['message']
+    key = int(data['key'])
+    method = data['method']
+
+    if method == 'caesar':
+        result = caesar_decrypt(message, key)
+
+    return jsonify({'decrypted_message': result})
+    
+if __name__ == '__main__':
+    app.run(debug = True)
