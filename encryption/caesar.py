1d2216942c31aa4b58199fb28682035d0a1b39a0
mstfkaradag <mustafakaradag20200@gmail.com>
2025-10-02 23:15:25 +0300

    Vigenere cipher coded

--- encryption/caesar.py
+++ encryption/caesar.py
@@ -4,7 +4,7 @@ def caesar_encrypt(text, shift):
         if char.isupper():
             result = result + chr((ord(char) + shift - 65) % 26 + 65) #ascii de büyük harfler 65-90 arası // chr -> bir sayıyı karaktere çevirir
         elif char.islower():
-            result = result + chr((ord(char) + shift -97) % 26 + 97) #ascii de küçük harfler 97-122 arası // ord -> karakterin ascii karşılığını verir
+            result = result + chr((ord(char) + shift - 97) % 26 + 97) #ascii de küçük harfler 97-122 arası // ord -> karakterin ascii karşılığını verir
         else:
             result = result + char
     
---
+++ encryption/vigenere.py
@@ -0,0 +1,25 @@
+def vigenere_encrypt(text, key):
+    result = ""
+    pattern = (key * ((len(text) // len(key)) + 1))[:len(text)]
+    for i, char in enumerate(text):
+        if char.isupper():
+            result = result + chr((ord(char) - 65 + (ord(pattern[i].upper()) - 65)) % 26 + 65)
+        elif char.islower():
+            result = result + chr((ord(char) - 97 + (ord(pattern[i].lower()) - 97)) % 26 + 97)
+        else:
+            result = result + char
+
+    return result
+
+def vigenere_decrypt(text, key):
+    result = ""
+    pattern = (key * ((len(text) // len(key)) + 1))[:len(text)]
+    for i, char in enumerate(text):
+        if char.isupper():
+            result = result + chr((ord(char) - 65 - (ord(pattern[i].upper()) - 65)) % 26 + 65)
+        elif char.islower():
+            result = result + chr((ord(char) - 97 - (ord(pattern[i].lower()) - 97)) % 26 + 97)
+        else:
+            result = result + char
+
+    return result
