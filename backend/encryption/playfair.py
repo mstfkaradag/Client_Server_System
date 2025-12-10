d4473de412fbfd9454c1a1888281774dcd57e8b2
mstfkaradag <mustafakaradag20200@gmail.com>
2025-12-10 22:17:33 +0300

    The errors in the Playfair cipher have been corrected

--- backend/encryption/playfair.py
+++ backend/encryption/playfair.py
@@ -1,11 +1,9 @@
 from .base import Cipher
-import math
 
 class Playfair(Cipher):
     def __init__(self, key):
         self.key = key.upper().replace("J", "I")
         self.alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-
         self.matrix = self._create_matrix(self.key)
 
     def _create_matrix(self, key):
@@ -22,7 +20,6 @@ class Playfair(Cipher):
             chunk = matrix_string[i: i + 5]
             row = list(chunk)
             result.append(row)
-
         return result
     
     def _get_pos(self, char):
@@ -30,7 +27,6 @@ class Playfair(Cipher):
             for col in range(5):
                 if self.matrix[row][col] == char:
                     return row, col
-                
         return None
     
     def _prepare_text(self, text):
@@ -39,15 +35,15 @@ class Playfair(Cipher):
         i = 0
         while i < len(text):
             a = text[i]
-
+            
             if i + 1 >= len(text):
-                result.append(a, 'X')
+                result.append((a, 'X'))
                 i += 1
             elif a == text[i + 1]:
-                result.append(a, 'X')
+                result.append((a, 'X'))
                 i += 1
             else:
-                result.append(a, text[i + 1])
+                result.append((a, text[i + 1]))
                 i += 2
         return result
 
@@ -80,8 +76,15 @@ class Playfair(Cipher):
             char1 = text[i]
             char2 = text[i + 1] if i + 1 < len(text) else 'X'
 
-            row1, col1 = self._get_pos(char1)
-            row2, col2 = self._get_pos(char2)
+            pos1 = self._get_pos(char1)
+            pos2 = self._get_pos(char2)
+
+            if not pos1 or not pos2:
+                decrypted_text.append(char1 + char2)
+                continue
+
+            row1, col1 = pos1
+            row2, col2 = pos2
 
             if row1 == row2:
                 col1 = (col1 - 1) % 5
